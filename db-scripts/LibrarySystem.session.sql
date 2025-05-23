764912315bb1248be28fa6210e6a70c5a63552eb
aetrig <emilia.gosztyla@gmail.com>
2025-05-23 16:08:29 +0200

    basic list of copies from selected book

--- db-scripts/LibrarySystem.session.sql
+++ db-scripts/LibrarySystem.session.sql
@@ -1,2 +1,20 @@
-SELECT title
-from books;
+INSERT INTO copies (
+		id_book,
+		id_publisher,
+		id_inventory,
+		year_published
+	)
+VALUES (1, 1, "TEST3", 1978);
+INSERT INTO copies (
+		id_book,
+		id_publisher,
+		id_inventory,
+		year_published
+	)
+VALUES (1, 1, "TEST4", 1978);
+INSERT INTO copies (
+		id_book,
+		id_publisher,
+		id_inventory,
+		year_published
+	)
--- db-scripts/SampleData.sql
+++ db-scripts/SampleData.sql
@@ -1,53 +1,111 @@
 -- ### Słupy do bazy ### --
-
 -- categories --
 -- Najwyższa kategoria 000
-INSERT INTO categories (id_category, id_supercategory, name, has_children, has_items) 
-VALUES ('000', null, 'Computer science, information, and general works', 1, 0);
-
+INSERT INTO categories (
+		id_category,
+		id_supercategory,
+		name,
+		has_children,
+		has_items
+	)
+VALUES (
+		'000',
+		null,
+		'Computer science, information, and general works',
+		1,
+		0
+	);
 -- Kategoria niższa 005
-INSERT INTO categories (id_category, id_supercategory, name, has_children, has_items) 
-VALUES ('005', '000', 'Computer programming, programs, and data', 1, 0);
-
+INSERT INTO categories (
+		id_category,
+		id_supercategory,
+		name,
+		has_children,
+		has_items
+	)
+VALUES (
+		'005',
+		'000',
+		'Computer programming, programs, and data',
+		1,
+		0
+	);
 -- Kategoria niższa 030
-INSERT INTO categories (id_category, id_supercategory, name, has_children, has_items) 
-VALUES ('030', '000', 'Encyclopedias and books of facts', 1, 0);
-
+INSERT INTO categories (
+		id_category,
+		id_supercategory,
+		name,
+		has_children,
+		has_items
+	)
+VALUES (
+		'030',
+		'000',
+		'Encyclopedias and books of facts',
+		1,
+		0
+	);
 -- Kategoria niższa 032
-INSERT INTO categories (id_category, id_supercategory, name, has_children, has_items) 
+INSERT INTO categories (
+		id_category,
+		id_supercategory,
+		name,
+		has_children,
+		has_items
+	)
 VALUES ('032', '030', 'Encyclopedias in English', 1, 0);
-
 -- publishers -- 
-INSERT INTO publishers (name) 
+INSERT INTO publishers (name)
 VALUES ('Encyclopædia Britannica, Inc.');
 INSERT INTO publishers (name)
 VALUES ('Addison-Wesley');
-
 -- authors --
 INSERT INTO authors (name, surname)
 VALUES ('Bjarne', 'Stroustrup');
 INSERT INTO authors (name, surname)
 VALUES ('Colin', 'Macfarquhar');
-
 -- books --
 INSERT INTO books (title, id_category, minimum_age)
-VALUES ('Programming : principles and practice using C++', '005', null);
-
+VALUES (
+		'Programming : principles and practice using C++',
+		'005',
+		null
+	);
 INSERT INTO books (title, id_category, minimum_age)
 VALUES ('Encyclopædia Britannica', '032', null);
-
 -- copies --
-INSERT INTO copies (id_book, id_publisher, id_inventory, year_published)
+INSERT INTO copies (
+		id_book,
+		id_publisher,
+		id_inventory,
+		year_published
+	)
 VALUES (1, 1, "TEST1", 1978);
-
-INSERT INTO copies (id_book, id_publisher, id_inventory, year_published)
+INSERT INTO copies (
+		id_book,
+		id_publisher,
+		id_inventory,
+		year_published
+	)
+VALUES (1, 1, "TEST3", 1978);
+INSERT INTO copies (
+		id_book,
+		id_publisher,
+		id_inventory,
+		year_published
+	)
+VALUES (1, 1, "TEST4", 1978);
+INSERT INTO copies (
+		id_book,
+		id_publisher,
+		id_inventory,
+		year_published
+	)
 VALUES (2, 2, "TEST2", 2002);
-
 -- clients --
 INSERT INTO clients (name, surname, PESEL)
 VALUES ('Jan', 'Kowalski', '02262505133');
-
 INSERT INTO clients (name, surname, PESEL)
 VALUES ('Anna', 'Nowak', '00222906666');
-
-INSERT INTO books_authors (id_book, id_author) VALUES (1, 1)
+INSERT INTO books_authors (id_book, id_author)
+VALUES (1, 1)
