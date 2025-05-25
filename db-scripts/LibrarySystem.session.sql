5e0d755807555071507951dcd2e2a24d5043f808
aetrig <emilia.gosztyla@gmail.com>
2025-05-25 18:09:23 +0200

    Removing client now also removes their rentals

--- db-scripts/LibrarySystem.session.sql
+++ db-scripts/LibrarySystem.session.sql
@@ -1,3 +1,4 @@
-SELECT *
-FROM rentals
-WHERE id_copy = 4
+INSERT INTO clients (id, name, surname, PESEL)
+VALUES (13, "test", "test", "test");
+INSERT INTO rentals (id_client, id_copy, date, deadline)
+VALUES (13, 1, "2025-03-02", "2025-04-02");
