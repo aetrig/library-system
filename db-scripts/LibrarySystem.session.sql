4fbd0c53b087c764b7e254f00f0a003e7abbb012
aetrig <emilia.gosztyla@gmail.com>
2025-05-25 20:38:50 +0200

    Adding and Removing books works

--- db-scripts/LibrarySystem.session.sql
+++ db-scripts/LibrarySystem.session.sql
@@ -1,4 +1,7 @@
-INSERT INTO clients (id, name, surname, PESEL)
-VALUES (13, "test", "test", "test");
-INSERT INTO rentals (id_client, id_copy, date, deadline)
-VALUES (13, 1, "2025-03-02", "2025-04-02");
+-- INSERT INTO clients (id, name, surname, PESEL)
+-- VALUES (13, "test", "test", "test");
+-- INSERT INTO rentals (id_client, id_copy, date, deadline)
+-- VALUES (13, 1, "2025-03-02", "2025-04-02");
+DELETE FROM books_authors
+WHERE id_book = 4
+	AND id_author = 5
