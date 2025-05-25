f8b11b8d91b7cb62d04667fd819c15effd769224
aetrig <emilia.gosztyla@gmail.com>
2025-05-25 15:16:14 +0200

    Made availability depend on rentals

--- db-scripts/LibrarySystem.session.sql
+++ db-scripts/LibrarySystem.session.sql
@@ -1,10 +1,3 @@
-INSERT INTO books (title, id_category, minimum_age)
-VALUES (
-		'Testowa Książka z wieloma autorami',
-		'000',
-		null
-	);
-INSERT INTO authors (name, surname)
-VALUES ('Imie1', 'Nazwisko1');
-INSERT INTO authors (name, surname)
-VALUES ('Imie2', 'Nazwisko2');
+SELECT *
+FROM rentals
+WHERE id_copy = 4
